b3422856051a3114ca216a4ee18a29abc31aa230
samuel <samuel@cygnus.com.uy>
2022-03-10 14:29:49 -0300

    Mas ajustes

--- lamp-ubuntu.sh
+++ lamp-ubuntu.sh
@@ -20,45 +20,57 @@ echo
 # Actualización del SO
 apt update -y
 
+read -p 'Instalar Apache2? [y/n]: ' r_apache2_opt
 ## Instalación de apache
-apt install apache2 -y
-ufw allow in "Apache"
-ufw status
+if ((opts_yes[$r_apache2_opt])); then
+  apt install apache2 -y
+  ufw allow in "Apache"
+  ufw status
+fi
 
+read -p 'Instalar Mysql? [y/n]: ' r_apache2_opt
 # Instalación de MySQL database server
-export DEBIAN_FRONTEND="noninteractive"
-debconf-set-selections <<<"mysql-server mysql-server/root_password password $db_root_password"
-debconf-set-selections <<<"mysql-server mysql-server/root_password_again password $db_root_password"
-apt install mysql-server -y
+if ((opts_yes[$r_apache2_opt])); then
+  export DEBIAN_FRONTEND="noninteractive"
+  debconf-set-selections <<<"mysql-server mysql-server/root_password password $db_root_password"
+  debconf-set-selections <<<"mysql-server mysql-server/root_password_again password $db_root_password"
+  apt install mysql-server -y
+fi
 
+read -p 'Instalar php 8.1? [y/n]: ' r_apache2_opt
 ## Instalación de PHP
-apt install software-properties-common && add-apt-repository ppa:ondrej/php -y
-apt update
-sudo apt upgrade -y
-apt install php8.1 libapache2-mod-php8.1 php8.1-mysql php8.1-fpm libapache2-mod-fcgid php-mbstring php-zip php-gd php-json php-curl-y
-a2enmod proxy_fcgi setenvif && a2enconf php8.1-fpm
-nano /etc/apache2/mods-enabled/dir.conf
-systemctl reload apache2
-systemctl status php8.1-fpm
-php --version
+if ((opts_yes[$r_apache2_opt])); then
+  apt install software-properties-common && add-apt-repository ppa:ondrej/php -y
+  apt update
+  sudo apt upgrade -y
+  apt install php8.1 libapache2-mod-php8.1 php8.1-mysql php8.1-fpm libapache2-mod-fcgid php-mbstring php-zip php-gd php-json php-curl -y
+  a2enmod proxy_fcgi setenvif && a2enconf php8.1-fpm
+  nano /etc/apache2/mods-enabled/dir.conf
+  systemctl reload apache2
+  systemctl status php8.1-fpm
+  php --version
 
-# Habilitación de Mod Rewrite
-a2enmod rewrite
-php5enmod mcrypt
+  # Habilitación de Mod Rewrite
+  a2enmod rewrite
+  php5enmod mcrypt
+fi
 
+read -p 'Instalar phpmyadmin? [y/n]: ' r_apache2_opt
 ## Instalación de PhpMyAdmin
-apt-get install wget zip unzip
-wget -P ~/ https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-all-languages.zip
-wget -P ~/ https://files.phpmyadmin.net/phpmyadmin.keyring
-gpg --import ~/phpmyadmin.keyring
-wget -P ~/ https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-all-languages.zip.asc
-gpg --verify ~/phpMyAdmin-5.1.3-all-languages.zip.asc
-mkdir /var/www/html/phpmyadmin
-unzip ~/phpMyAdmin-5.1.3-all-languages.zip -d /var/www/html/phpmyadmin
-cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
-nano /var/www/html/phpmyadmin/config.inc.php
-chmod 660 /var/www/html/phpmyadmin/config.inc.php
-chown -R www-data:www-data /var/www/html/phpmyadmin
+if ((opts_yes[$r_apache2_opt])); then
+  apt-get install wget zip unzip -y
+  wget -P ~/ https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-all-languages.zip
+  wget -P ~/ https://files.phpmyadmin.net/phpmyadmin.keyring
+  gpg --import ~/phpmyadmin.keyring
+  wget -P ~/ https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-all-languages.zip.asc
+  gpg --verify ~/phpMyAdmin-5.1.3-all-languages.zip.asc
+  unzip ~/phpMyAdmin-5.1.3-all-languages.zip -d /var/www/html
+  mv /var/www/html/phpMyAdmin-5.1.3-all-languages /var/www/html/phpmyadmin
+  cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
+  nano /var/www/html/phpmyadmin/config.inc.php
+  chmod 660 /var/www/html/phpmyadmin/config.inc.php
+  chown -R www-data:www-data /var/www/html/phpmyadmin
+fi
 
 # Configuracion general
 chown -R www-data:www-data /var/www
